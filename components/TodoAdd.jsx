2e046da1f703779b48faa89888dc6e2b20bf2beb
Florian Müllner <florian.muellner@stud.fh-campuswien.ac.at>
2024-01-07 19:27:26 +0100

    add darkmode and name

--- components/TodoAdd.jsx
+++ components/TodoAdd.jsx
@@ -1,7 +1,7 @@
 import { Button, Grid, TextField } from "@mui/material";
 import React from "react";
 
-export default function TodoAdd (props) {
+export default function TodoAdd(props) {
 
 
     const { onCreate } = props;
@@ -9,26 +9,32 @@ export default function TodoAdd (props) {
     const [inputValue, setInputValue] = React.useState("");
     return (
         <Grid container>
-                <Grid  md={11} item style={{ paddingRight: 16 }}>
-                    <TextField
-                        placeholder="Add Todo here"
-                        value={inputValue}
-                        onChange={v => setInputValue(v.target.value)}
-                        fullWidth                 
-                    />
-                </Grid>
-                <Grid  md={1} item>
-                    <Button
-                        color="info"
-                        variant="outlined"
-                        onClick={() => onCreate(inputValue)}
-                        sx={{
-                            height: 55,
-                        }}
-                    >
-                        Add
-                    </Button>
-                </Grid>
+            <Grid md={11} item style={{ paddingRight: 16 }}>
+                <TextField
+                    variant="filled"
+                    color="text.primary"
+                    placeholder="Add Todo here"
+                    value={inputValue}
+                    onChange={v => setInputValue(v.target.value)}
+                    fullWidth
+                    sx={{
+                        borderColor: 'secondary.main',
+                        bgcolor: 'primary.main'
+                    }}
+                />
+            </Grid>
+            <Grid md={1} item>
+                <Button
+                    color="info"
+                    variant="outlined"
+                    onClick={() => onCreate(inputValue)}
+                    sx={{
+                        height: 55,
+                    }}
+                >
+                    Add
+                </Button>
+            </Grid>
         </Grid>
     )
 
